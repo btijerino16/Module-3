c143ea674c963a5a61d3a5527102814e6a8ffcec
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-11-21 20:00:50 -0700

    arranging arrays and fomratting documents to make sense

--- Develop/script.js
+++ Develop/script.js
@@ -3,30 +3,27 @@
 function generatePassword () {
 
 
-var randomLowerCase = ["a","b","c","d","f","g","f","h","i","o","p","q","r","s","t","u","v","w","x","y","z"]
-var randomUpperCase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var randomNumber = ["1","2","3","4","5","6","7","8","9","0"]
-var randomSymbols = ["!","@","#","$","%","^","&","*"]
+var randomLowerCaseArray = ["a","b","c","d","f","g","f","h","i","o","p","q","r","s","t","u","v","w","x","y","z"]
+var randomUpperCaseArray =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var randomNumberArray = ["1","2","3","4","5","6","7","8","9","0"]
+var randomSymbolsArray = ["!","@","#","$","%","^","&","*"]
 
 
 //Prompts for password generator
 
-var confirmLengthArray = prompt("Please choose between 8-128 characters");
+var confirmLength = prompt ("Please choose between 8-128 characters");
 // choice of numbers
-var numberschoiceArray = confirm("Would you like to add numbers?");
+var numberschoice = confirm ("Would you like to add numbers?");
 // choice of lower case
-var lowercaseArray = confirm("Would you like to add lowercase letters?");
+var lowercase = confirm ("Would you like to add lowercase letters?");
 // choice of upercase
-var uppercaseArray = confirm("Would you like to add uppercase letters?");
+var uppercase = confirm ("Would you like to add uppercase letters?");
 // choice of symbols
-var specialcharactersArray = confirm("Would you like to add special characters?");
+var specialcharacters = confirm ("Would you like to add special characters?");
 
 
-}
 
 // if statements for confirm
-
-
 
 
 // Get references to the #generate element
@@ -40,14 +37,6 @@ function writePassword() {
 
   passwordText.value = password;
   
-}
 
 // Add event listener to generate button
 function password() generateBtn.addEventListener("click", writePassword); 
-}
-//arrays
-
-var resultArray = [];
-var userArray = [];
-
-uppercaseArray [1]
